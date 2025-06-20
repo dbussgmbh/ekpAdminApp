7873eaefcd587fbd01418607ba717d0442fe0b26
Michael Quaschny <m.quaschny@dbuss.de>
2025-06-20 13:48:08 +0200

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+package de.dbuss.ekpadminapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/Login.fxml"));
+
+
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
